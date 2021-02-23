7a5ed887c3277b80172848a8acb425ac17d1a7cf
Peter Creyghton <peter@convexity.nl>
2021-02-23 22:17:37 +0100

    removed wrong option from ssh-keygen

--- make_keys.sh
+++ make_keys.sh
@@ -5,7 +5,7 @@ KEY=signkey
 echo "KEY=$KEY">keyname
 
 # generate keypair
-ssh-keygen -t rsa -f $KEY -b 4096 -N "" -A
+ssh-keygen -t rsa -f $KEY -b 4096 -N "" &>/dev/null
 
 # generate pem file for public key
 openssl pkey -in $KEY -pubout -out $KEY.pem
